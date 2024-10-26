fcf5b0240f9648544d1e5783f61977321a2dcf70
Damian Wasiak <dkfishy@o2.pl>
2024-10-26 12:20:05 +0200

    Modified models to use factories

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+	use HasFactory;
+	
     protected $fillable = ['post_id', 'user_id', 'comment'];
 
     public function post()
